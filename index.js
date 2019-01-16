892ed2a878fdcb3b2099b89c758387c38a84c06a
beatific-demo-1566 <sbn.houghton@gmail.com>
2019-01-16 04:47:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line, name){
-  line = line.push(name)
+  line.push(name)
   var welcomeMessage = `Welcome, ${name}. You are number ${line.length} in line.`;
   
   return welcomeMessage
